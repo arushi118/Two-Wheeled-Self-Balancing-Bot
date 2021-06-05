c00edd8ee16687f8434a61fb6dd774b278784d0b
arushi118 <arushig20@iitk.ac.in>
2021-06-05 19:24:36 +0530

    Assignment

---
+++ Assignment/Assignment 1/Assignment1_const.m
@@ -0,0 +1,7 @@
+m1=10;                %Mass 1[kg]
+m2=100;               %Mass 2[kg]
+c1=100;               %Damping Coefficient 1[Ns/m]
+c2=1000;              %Damping Coefficient 2[Ns/m]         
+k1=1e4;               %Spring Coefficient 1[N/m]
+k2=1e5;               %Spring Coefficient 2[N/m]
+Tend=10;              %Simulation Stop Time[s]
